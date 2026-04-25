6482009d8f55ea5173692983751537a2f84e98a2
scratch221171 <scratch221171@gmail.com>
2026-04-25 21:13:46 +0900

    クラフトイベントのテスト データ駆動にしたほうがいいのかな...

---
+++ src/main/java/net/scratch221171/astralenchant/common/event/AERecipeEventHandler.java
@@ -0,0 +1,46 @@
+package net.scratch221171.astralenchant.common.event;
+
+import net.minecraft.tags.ItemTags;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.item.ItemEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.level.ExplosionEvent;
+import net.scratch221171.astralenchant.common.AstralEnchant;
+
+import java.util.Iterator;
+
+@EventBusSubscriber(modid = AstralEnchant.MOD_ID)
+public class AERecipeEventHandler {
+
+    @SubscribeEvent
+    private static void onExplode(ExplosionEvent.Detonate event) {
+        Level level = event.getLevel();
+        if (level.isClientSide) return;
+        Iterator<Entity> it = event.getAffectedEntities().iterator();
+        while (it.hasNext()) {
+            Entity e = it.next();
+            if (e instanceof ItemEntity item && item.isAlive()) {
+                if (processExplosionRecipe(item, event)) {
+                    it.remove();
+                    item.discard();
+                }
+            }
+        }
+    }
+
+    static boolean processExplosionRecipe(ItemEntity itemEntity, ExplosionEvent.Detonate event) {
+        if (itemEntity.getItem().is(ItemTags.COALS)) {
+            Level level = event.getLevel();
+            level.addFreshEntity(new ItemEntity(
+                    level, itemEntity.getX(), itemEntity.getY(), itemEntity.getZ(),
+                    new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount())
+            ));
+            return true;
+        }
+        return false;
+    }
+}
